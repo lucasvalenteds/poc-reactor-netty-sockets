a911219df3b0bc6eae62a8e6795f8f2c714923ab
Lucas <lucasvalenteds@gmail.com>
2021-07-06 18:59:42 -0300

    Refactoring HeaderHandler
    Replacing if/else statements with Reactor operators.

--- src/main/java/io/lucasvalenteds/spring/reactive/ws/HeaderHandler.java
+++ src/main/java/io/lucasvalenteds/spring/reactive/ws/HeaderHandler.java
@@ -4,23 +4,21 @@ import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.websocket.WebsocketInbound;
 import reactor.netty.http.websocket.WebsocketOutbound;
 
-import java.util.Optional;
 import java.util.function.BiFunction;
 
 public class HeaderHandler implements BiFunction<WebsocketInbound, WebsocketOutbound, Publisher<Void>> {
 
     @Override
     public Publisher<Void> apply(WebsocketInbound in, WebsocketOutbound out) {
-        String header = in.headers().getAsString(HttpHeaderNames.CONTENT_TYPE);
-        String contentType = Optional.ofNullable(header).orElse(HttpHeaderValues.TEXT_PLAIN.toString());
-
-        if (contentType.equals(HttpHeaderValues.TEXT_PLAIN.toString())) {
-            return out.sendString(Flux.just("Hello World!"));
-        } else {
-            return out.sendString(Flux.just("{\"message\":\"Hello World!\"}"));
-        }
+        return Mono.just(in.headers())
+            .map(headers -> headers.getAsString(HttpHeaderNames.CONTENT_TYPE))
+            .filter(contentType -> contentType.equals(HttpHeaderValues.TEXT_PLAIN.toString()))
+            .flatMap(it -> Mono.just("Hello World!"))
+            .switchIfEmpty(Mono.just("{\"message\":\"Hello World!\"}"))
+            .flatMap(responseBody -> out.sendString(Flux.just(responseBody)).then());
     }
 }
